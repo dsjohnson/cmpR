789fdbf4e8c1cedc6bab5a22ff421b4370d55a0a
Devin Johnson <devin.johnson@noaa.gov>
2017-05-15 14:09:59 -0700

    First commit

---
+++ R/dcmp.R
@@ -0,0 +1,42 @@
+#' @title Conway-Maxwell-Poisson Distribution
+#' @aliases rcmp
+#' @rdname cmp
+#' @description Probability mass function and random generation for the
+#' Conway-Maxwell-Poisson distribution for given values of the parameters.
+#' @param x quantile at which the prob. mass function is evaluated.
+#' @param mu location parameter
+#' @param nu dispersion parameter
+#' @param log Logical. Whether or not the log PMF is returned.
+#' @param n number of random draws to return
+#'
+#' @details Computes PMF and makes random draws from the Conway-Maxwell-Poisson (CMP)
+#' distribution. The PMF of the CMP is given by
+#' \deqn{f(x) = (1/Z) ((nu*mu)^x)/(x!^nu).}
+#' (Guikema and Goffelt 2008).
+#'
+#' The normalizing constant is calculated using a combination of finite truncation of the infinite sum
+#' as well as an approximation provided by Shmueli et al. (2005) for small nu and large mu.
+#' @author Devin S. Johnson and Jeffrey Dunn
+#' @references Guikema, Seth D., and Jeremy P. Goffelt. "A flexible count data regression model for risk analysis." Risk analysis 28.1 (2008): 213-223.
+#'
+#' Shmueli, Galit, et al. "A useful distribution for fitting discrete data: revival of the Conway–Maxwell–Poisson distribution." Journal of the Royal Statistical Society: Series C (Applied Statistics) 54.1 (2005): 127-142.
+#' @export
+
+dcmp = function(x, mu, nu, log=FALSE){
+  if(length(mu)==1){
+    mu = rep(mu, length(x))
+  } else if(length(mu!=length(x))) stop("Length of mu vector must equal 1 or length(x)")
+  if(length(nu)==1){
+    mu = rep(nu, length(x))
+  } else if(length(nu!=length(x))) stop("Length of mu vector must equal 1 or length(x)")
+  ff = cmp_log_pmf(x, mu, nu)
+  if(log) return (ff)
+  else return(exp(ff))
+}
+
+#' @author Devin S. Johnson and Jeffrey Dunn
+#' @export
+rcmp = function(n, mu, nu) {
+  if (mu < 0 || nu < 0) return(rep(NaN,n))
+  else(return(samp_cmp(n, mu, nu)))
+}
---
+++ src/cmp_funcs.cpp
@@ -0,0 +1,84 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+
+double com_log_sum(double x, double y){
+  if(x==R_NegInf){return y;}
+  else if(y==R_NegInf){return x;}
+  else if(x > y){
+    return x + log(1 + exp(y - x));
+  } else{
+    return y + log(1 + exp(x - y));
+  }
+}
+
+double compute_log_z(const double& mu, const double& nu,
+                     const double& log_eps, const bool& approx) {
+  double ln_z;
+  if(mu<=0 | nu<0){ln_z = R_NaN;}
+  else if(nu==0){
+     if(mu<1) ln_z = -log(1-pow(mu,nu));
+     else ln_z=R_NaN;
+  }
+  else{
+    if(approx){
+      ln_z = nu*mu - (nu-1)*log(mu)/(nu) - (nu-1)*log(2*PI)/2 - log(nu)/2;
+    } else{
+      ln_z = R_NegInf;
+      double ln_z_last = 0;
+      int j = 0;
+      while(std::abs(ln_z - ln_z_last) > log_eps){
+        ln_z_last = ln_z;
+        ln_z = com_log_sum(ln_z, j*nu*log(mu) - nu*lgamma(j+1));
+        j = j+1;
+      }
+    }
+  }
+  return ln_z;
+}
+
+double log_z(const double& mu, const double& nu){
+  double out = compute_log_z(mu, nu, 1.0E-8, true);
+  if(out<2000) out = compute_log_z(mu, nu, 1.0E-8, false);
+  return out;
+}
+
+double cmp_log_pmf(const int& x, const double& mu, const double& nu){
+  double out;
+    if(x<0){out = R_NaN;}
+    else{
+      out = x*nu*log(mu) - nu*lgamma(x+1) - log_z(mu,nu);
+    }
+  return out;
+}
+
+// [[Rcpp::export]]
+NumericVector ln_dcmp(const NumericVector& x, const NumericVector& mu, const NumericVector& nu){
+  NumericVector out(x.size());
+  for(int i=0; i<out.size(); i++){
+    out(i) = cmp_log_pmf(x(i), mu(i), nu(i));
+  }
+  return out;
+}
+
+// [[Rcpp::export]]
+IntegerVector samp_cmp(const int& n, const double& mu, const double& nu){
+  IntegerVector out(n);
+  double U;
+  double ln_cmf;
+  double ln_cmf_last;
+  double ln_pmf;
+  int x;
+
+  for(int i=0; i<n; i++){
+    U = Rcpp::as<double>(Rcpp::runif(1));
+    ln_cmf = R_NegInf;
+    x= -1;
+    while(ln_cmf<=log(U)){
+      x += 1;
+      ln_cmf = com_log_sum(ln_cmf, cmp_log_pmf(x, mu, nu));
+    }
+    out(i) = x;
+  }
+  return out;
+}
